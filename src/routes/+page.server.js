4d82d2a789700719980f4246fcc1c1c06ae52697
Isaac Saunders <git@eartharoid.me>
2026-08-17 03:44:49 +0100

    polyfill?

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,6 @@
 import { env } from '$env/dynamic/private';
-
+import { DOMParser } from '@xmldom/xmldom';
+globalThis.DOMParser = DOMParser;
 import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3';
 
 const s3 = new S3Client({
